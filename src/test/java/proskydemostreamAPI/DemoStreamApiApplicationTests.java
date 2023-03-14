fb378e151d4ccd8176a5452c760634716ff2428f
Andrey.Lutskiy <luzkiyac@mail.ru>
2023-03-14 12:02:49 +0300

    Домашняя работа по стриму, добавил pom.xml

---
+++ src/test/java/proskydemostreamAPI/DemoStreamApiApplicationTests.java
@@ -0,0 +1,11 @@
+package proskydemostreamAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoStreamApiApplicationTests {
+
+
+
+}
